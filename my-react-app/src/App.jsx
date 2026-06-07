e1d2adda74f6b559d35de407bebe18d72f5ba8c9
anshikakalhans <anshikaakalhans@gmail.com>
2026-06-07 23:54:14 +0530

    react flow and structure

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,13 @@
+import Test from "./Test";
+
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <Test />
+      <h1> testing react </h1>
+      <p> test para </p>
+    </>
+  );
 }
 
 export default App;
-
---
+++ my-react-app/src/Test.jsx
@@ -0,0 +1,5 @@
+function Test() {
+  return <h2> test is live </h2>;
+}
+
+export default Test;
